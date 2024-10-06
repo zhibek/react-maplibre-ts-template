85735260e2adb78e60892849c54d5a4c636e941e
John Levermore <john.levermore@zhibek.com>
2024-10-06 15:14:26 +0100

    feat: Setup maplibre-gl & react-map-gl & implement basic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import Map, { Marker, NavigationControl } from 'react-map-gl/maplibre';
+
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 function App() {
+  const location = {
+    latitude: 51.5078,
+    longitude: -0.128,
+  };
+
+  const zoom = 4;
+
+  const mapStyleUrl = 'https://demotiles.maplibre.org/style.json';
+
   return (
-    <h1>React MapLibre TypeScript Template</h1>
+    <Map
+      initialViewState={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+        zoom,
+      }}
+      mapStyle={mapStyleUrl}
+      hash
+    >
+      <NavigationControl />
+      <Marker
+        latitude={location.latitude}
+        longitude={location.longitude}
+        color="red"
+      />
+    </Map>
   );
 }
 
