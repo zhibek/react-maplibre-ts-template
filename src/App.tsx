1438c27eb9e025750ccab25dff1a37251268efb3
John Levermore <john.levermore@zhibek.com>
2024-10-17 11:56:40 +0100

    feat: Implement layout with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import Map, { Marker, NavigationControl } from 'react-map-gl/maplibre';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1769aa',
+      },
+      secondary: {
+        main: '#ab003c',
+      },
+    },
+  });
+
   const location = {
     latitude: 51.5078,
     longitude: -0.128,
@@ -10,25 +33,61 @@ function App() {
 
   const zoom = 4;
 
-  const mapStyleUrl = 'https://demotiles.maplibre.org/style.json';
+  const mapStyleUrl = 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
 
   return (
-    <Map
-      initialViewState={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        zoom,
-      }}
-      mapStyle={mapStyleUrl}
-      hash
-    >
-      <NavigationControl />
-      <Marker
-        latitude={location.latitude}
-        longitude={location.longitude}
-        color="red"
-      />
-    </Map>
+    <ThemeProvider theme={theme}>
+
+      <CssBaseline />
+
+      <AppBar position="static">
+        <Toolbar>
+          Map
+        </Toolbar>
+      </AppBar>
+
+      <Grid
+        container
+        style={{ height: '100%' }}
+      >
+
+        <Grid
+          size={{ xs: 12, md: 9 }}
+          order={{ xs: 1, md: 2 }}
+          minHeight="70%"
+        >
+          <Map
+            initialViewState={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+              zoom,
+            }}
+            mapStyle={mapStyleUrl}
+            hash
+          >
+            <NavigationControl />
+            <Marker
+              latitude={location.latitude}
+              longitude={location.longitude}
+              color="red"
+            />
+          </Map>
+        </Grid>
+
+        <Grid
+          size={{ xs: 12, md: 3 }}
+          order={{ xs: 2, md: 1 }}
+          minHeight="30%"
+          style={{ background: 'lightgray' }}
+        >
+          <Box margin={3}>
+            <p>Panel Data</p>
+          </Box>
+        </Grid>
+
+      </Grid>
+
+    </ThemeProvider>
   );
 }
 
